81a59e82a7b9134478ea49a79834dfaaa93d7640
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-05-22 19:34:33 -0300

    Add aula-6 content

---
+++ aula-6/app.js
@@ -0,0 +1,7 @@
+const numbers = [2, 3, 5, 7];
+const numbersBy2 = numbers.map((v) => v * 2);
+console.log(numbersBy2);
+
+const lowercasedValues = ["oi", "tudo", "bem?"];
+const uppercasedValues = lowercasedValues.map((v) => v.toUpperCase());
+console.log(uppercasedValues);
